73d334b295775c695511d3e96af09e0ca7ea7e2f
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2026-01-31 19:53:55 -0600

    Add 404 handling for RecordNotFound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
   before_action :require_login
 
   helper_method :current_user, :logged_in?
@@ -19,5 +21,9 @@ class ApplicationController < ActionController::Base
     unless logged_in?
       redirect_to login_path, alert: "Please log in to continue"
     end
+  end
+
+  def not_found
+    render "errors/not_found", status: :not_found
   end
 end
